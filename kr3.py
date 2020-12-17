4d6bc8d5c7e742df8444a786c145bd25351d2e70
Nikolai Zakharevich <n.zakharevich@corp.vk.com>
2020-12-17 06:50:02 +0300

    refactored

--- kr3.py
+++ kr3.py
@@ -1,43 +1,49 @@
 import numpy as np
 import convcode as cc
 from sympy import *
+import urllib.parse
 
-# memory = np.array([2])
-# g_matrix = np.array([[1, 3, 7]])
+MEMORY = np.array([2])
+G = np.array([[5, 7]])
 
-# memory = np.array([2])
-# g_matrix = np.array([[5, 7]])
+# MEMORY = np.array([2])
+# G = np.array([[1, 3, 7]])
 
-memory = np.array([3])
-g_matrix = np.array([[13, 11]])
+# MEMORY = np.array([3])
+# G = np.array([[13, 11]])
 
-trellis = cc.Trellis(memory, g_matrix)
-n = trellis.number_states
+WOLFRAM_ALPHA_URL = 'https://www.wolframalpha.com/input/?i='
 
-trellis.visualize()
 
-equations = []
-g_all = symbols('g0:%d' % n)
-d = symbols('D')
-i = symbols('I')
-for state_to in range(n):
-    equation = -g_all[state_to]
-    for state_from in range(n):
-        if not np.count_nonzero(trellis.next_state_table[state_from, :] == state_to) or (
-                state_to == 0 and state_from == 0):
-            continue
-        bit = np.where(trellis.next_state_table[state_from, :] == state_to)[0][0]
-        weight = int(trellis.output_table[state_from, bit])
-        x = 1
-        if state_from != 0:
-            x = g_all[state_from]
-        if bit == 1:
-            x *= i
-        equation += x * d ** bin(weight).count('1')
+def get_wolfram_link(input: str) -> str:
+    return WOLFRAM_ALPHA_URL + urllib.parse.quote_plus(input)
 
-    equations.append(equation)
 
-print(equations)
-g0_function = linsolve(equations, g_all).args[0][0]
+if __name__ == '__main__':
+    trellis = cc.Trellis(MEMORY, G)
+    n = trellis.number_states
+    # trellis.visualize()
 
-print(g0_function)
+    equations = []
+    g_all = symbols('g0:%d' % n)
+    d = symbols('D')
+    i = symbols('I')
+    for state_to in range(n):
+        equation = -g_all[state_to]
+        for state_from in range(n):
+            if not np.count_nonzero(trellis.next_state_table[state_from, :] == state_to) or (
+                    state_to == 0 and state_from == 0):
+                continue
+            bit = np.where(trellis.next_state_table[state_from, :] == state_to)[0][0]
+            weight = int(trellis.output_table[state_from, bit])
+            x = 1
+            if state_from != 0:
+                x = g_all[state_from]
+            if bit == 1:
+                x *= i
+            equation += x * d ** bin(weight).count('1')
+        equations.append(equation)
+
+    g0_function = linsolve(equations, g_all).args[0][0]
+    # Заменил I на Y, чтобы вольфрам не считал это мнимой единицей
+    print(get_wolfram_link("series " + str(g0_function).replace('**', '^').replace('I', 'Y')))
